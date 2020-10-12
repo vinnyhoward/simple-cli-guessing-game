999e72744752859c2d2690ddc0f6131bd7575ee1
Vince Howard <vincenguyenhoward@gmail.com>
2020-10-12 11:23:12 -0600

    finished first cli game using rust

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,28 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
